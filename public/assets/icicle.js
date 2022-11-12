c53241a76114a673065ae85cbbe9662659b1717b
Nathan Engler <englernathan@gmail.com>
2022-11-12 07:04:58 -0600

    removed unused files

--- public/assets/icicle.js
+++
@@ -1,7 +0,0 @@
-export default function Icicle() {
-  return (
-    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4">
-      <polygon points="12,0 24,12 12,24 0,12" stroke="#b2cada" fill="#b2cada"></polygon>
-    </svg>
-  );
-}
